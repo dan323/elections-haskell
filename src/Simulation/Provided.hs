28f44dfea2598f322a7a0290f81fb4c8010ff105
dan232 <danconsa@hotmail.com>
2020-08-22 19:23:18 +0200

    Fixed values simulation added

--- src/Simulation/Provided.hs
+++ src/Simulation/Provided.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE ExplicitForAll #-}
-module Simulation.Provided (limitSim) where
+module Simulation.Provided (limitSim, fixedSim) where
 
 import           Simulation.Simulation
 
-import qualified Data.Map  as M
+import qualified Data.Map             as M
 import           System.Random
 
 limitSim :: Ord k => Int -> Integer -> Integer -> Simulation k
@@ -11,3 +11,6 @@ limitSim gen min max lst = M.fromList (zip lst vals)
     where
         rnd  = mkStdGen gen
         vals = randomRs (min,max) rnd
+
+fixedSim :: Ord k => (k -> Integer) -> Simulation k
+fixedSim fun lst = M.fromList [(k,fun k)| k <- lst]
