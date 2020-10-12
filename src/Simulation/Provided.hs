7ee9d3defaf570cf202e37fa4aa2f31e10c5cc54
dan232 <danconsa@hotmail.com>
2020-10-12 10:36:51 +0200

    more style

--- src/Simulation/Provided.hs
+++ src/Simulation/Provided.hs
@@ -1,23 +1,23 @@
 {-# LANGUAGE ExplicitForAll #-}
 module Simulation.Provided (limitSim, fixedSim) where
 
-import           Simulation.Simulation
 import           Data.Votes
+import           Simulation.Simulation
 
 import qualified Data.Map              as M
 import           System.Random
 
 limitSim :: Ord k =>
-            Int -> 
-            Integer -> 
-            Integer -> 
+            Int ->
+            Integer ->
+            Integer ->
             Simulation k
 limitSim gen min max lst = (Votes . M.fromList) (zip lst vals)
     where
         rnd  = mkStdGen gen
         vals = randomRs (min,max) rnd
 
-fixedSim :: Ord k => 
-            (k -> Integer) -> 
+fixedSim :: Ord k =>
+            (k -> Integer) ->
             Simulation k
 fixedSim fun lst = (Votes . M.fromList) [(k,fun k)| k <- lst]
